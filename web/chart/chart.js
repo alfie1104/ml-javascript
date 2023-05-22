2a7953c36af967765e60a8473cf08cb34375c6e3
peter_alfie <alfie1104@gmail.com>
2023-05-22 15:48:11 +0900

    model evaluation

--- web/chart/chart.js
+++ web/chart/chart.js
@@ -218,6 +218,7 @@ class Chart {
       );
 
       graphics.drawPoint(ctx, pixelLoc, "rgba(255,255,255,0.7", 10000000);
+      ctx.strokeStyle = "gray";
       for (const sample of this.nearestSamples) {
         const point = math.remapPoint(
           this.dataBounds,
