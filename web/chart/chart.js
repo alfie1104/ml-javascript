7b350c1227852f8e1b18f363b298c192c456210f
peter_alfie <alfie1104@gmail.com>
2023-05-22 14:39:13 +0900

    nearest neighbo classifier

--- web/chart/chart.js
+++ web/chart/chart.js
@@ -37,14 +37,16 @@ class Chart {
     this.defaultDataBounds = this.#getDataBounds();
 
     this.dynamicPoint = null;
+    this.nearestSample = null;
 
     this.#draw();
 
     this.#addEventListeners();
   }
 
-  showDynamicPoint(point) {
-    this.dynamicPoint = point;
+  showDynamicPoint(point, label, nearestSample) {
+    this.dynamicPoint = { point, label };
+    this.nearestSample = nearestSample;
     this.#draw();
   }
 
@@ -177,10 +179,14 @@ class Chart {
     const maxX = Math.max(...x);
     const minY = Math.min(...y);
     const maxY = Math.max(...y);
+    const deltaX = maxX - minX;
+    const deltaY = maxY - minY;
+    const maxDelta = Math.max(deltaX, deltaY);
+
     const bounds = {
       left: minX,
-      right: maxX,
-      top: maxY,
+      right: maxX, //minX + maxDelta,
+      top: maxY, //minY + maxDelta,
       bottom: minY,
     };
     return bounds;
@@ -203,13 +209,27 @@ class Chart {
     }
 
     if (this.dynamicPoint) {
+      const { point, label } = this.dynamicPoint;
+
       const pixelLoc = math.remapPoint(
         this.dataBounds,
         this.pixelBounds,
-        this.dynamicPoint
+        point
       );
+
       graphics.drawPoint(ctx, pixelLoc, "rgba(255,255,255,0.7", 10000000);
-      graphics.drawPoint(ctx, pixelLoc, "black");
+      ctx.beginPath();
+      ctx.moveTo(...pixelLoc);
+      ctx.lineTo(
+        ...math.remapPoint(
+          this.dataBounds,
+          this.pixelBounds,
+          this.nearestSample.point
+        )
+      );
+      ctx.stroke();
+
+      graphics.drawImage(ctx, this.styles[label].image, pixelLoc);
     }
 
     this.#drawAxes();
